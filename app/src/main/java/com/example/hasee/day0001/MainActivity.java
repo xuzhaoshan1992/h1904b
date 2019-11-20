2a97e7770d4c4807231d8e9fe45ce48d233d8e26
zhou-XianSheng <2949870802@qq.com>
2019-11-20 18:10:30 +0800

    333

---
+++ app/src/main/java/com/example/hasee/day0001/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hasee.day0001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void test(){
+        System.out.println("ll");
+    }
+}
